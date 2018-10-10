d09993588a44d937ae9f1a56dcdcec745ca770e0
Daniel Clasen <mail@danielclasen.de>
2018-10-10 20:18:30 +0200

    remove needless Controller interface

--- controller/controller.go
+++ controller/controller.go
@@ -2,10 +2,6 @@ package controller
 
 import "github.com/gin-gonic/gin"
 
-type Controller interface {
-	Routes() *gin.RouterGroup
-}
-
 func handleError(c *gin.Context, err error, statusCode int) bool {
 	if err != nil {
 		c.JSON(statusCode, gin.H{"error": err.Error()})
